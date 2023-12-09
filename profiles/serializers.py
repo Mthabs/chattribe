46651d7e2f9a3f682055609913ebc7f9565588fc
Mthabs <mthabs1@gmail.com>
2023-12-09 23:53:23 +0000

    add profiles detail view

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,9 +2,21 @@ from rest_framework import serializers
 from .models import UserProfile
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    user_id = serializers.ReadOnlyField(source='user.id')
+    user_id = serializers.ReadOnlyField(source='id')
     user = serializers.ReadOnlyField(source='user.username')
 
     class Meta:
         model = UserProfile
         fields = ['user_id', 'user', 'created_at', 'updated_at', 'bio', 'content', 'profile_picture', 'cover_photo']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        default_profile_picture_url = 'https://res.cloudinary.com/dnt7oro5y/image/upload/v1702078965/default_profile_yansvo.jpg'
+        default_cover_photo_url = 'https://res.cloudinary.com/dnt7oro5y/image/upload/v1702078965/default_profile_ifketo.jpg'
+        if not instance.profile_picture:
+            representation['profile_picture'] = default_profile_picture_url
+
+        if not instance.cover_photo:
+            representation['cover_photo'] = default_cover_photo_url
+
+        return representation
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,27 @@
 from rest_framework import generics
+from django.shortcuts import get_object_or_404
 from .models import UserProfile
 from .serializers import UserProfileSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 class ProfileList(generics.ListAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        profile = get_object_or_404(UserProfile, pk=kwargs['pk'])
+        serializer = self.get_serializer(profile)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        profile = self.get_object()
+        serializer = self.get_serializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
