12060e2d63de0b3fad82f3aae0bb72a3579ff162
Mthabs <mthabs1@gmail.com>
2023-12-10 15:13:54 +0000

    add created posts serializer, default post function and validate

--- posts/models.py
+++ posts/models.py
@@ -2,13 +2,31 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    post_picture = models.ImageField(upload_to='post_images/', null=True, blank=True)
+    IMAGE_FILTER_CHOICES = [
+        ('normal', 'Normal'),
+        ('black_and_white', 'Black and White'),
+        ('sepia', 'Sepia'),
+        ('vintage', 'Vintage'),
+        ('grayscale', 'Grayscale'),
+        ('warm', 'Warm Tone'),
+        ('cool', 'Cool Tone'),
+        ('invert', 'Invert Colors'),
+        ('blur', 'Blur'),
+        ('sharpen', 'Sharpen'),
+    ]
     header = models.CharField(max_length=255, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_picture = models.ImageField(upload_to='post_images/', null=True, blank=True)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    image_filter = models.CharField(
+        max_length=32,
+        choices=IMAGE_FILTER_CHOICES,
+        default='normal',
+        blank=True,
+        null=True
+    )
     class Meta:
         verbose_name = "Post"
         verbose_name_plural = "Posts"
---
+++ posts/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    user_id = serializers.ReadOnlyField(source='user.id')
+	user = serializers.ReadOnlyField(source='user.username')
+    profile_id = serializers.ReadOnlyField(source='user.userprofile.id')
+    profile_picture = serializers.ReadOnlyField(source='user.userprofile.profile_picture.url')
+    is_owner = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Post
+        fields = ['id', 'user_id', 'profile_id', 'user', 'content', 'post_picture', 'header', 'created_at', 'updated_at', 'is_owner', 'profile_picture']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        default_post_picture_url = 'https://res.cloudinary.com/dnt7oro5y/image/upload/v1702078965/default_post_wv7bvz.jpg'
+        
+        if not instance.post_picture:
+            representation['post_picture'] = default_post_picture_url
+
+        return representation
+
+	def validate_post_picture(self, value):
+		max_size = 2 * 1024 * 1024  # 2 MB
+		if value.size > max_size:
+			raise serializers.ValidationError('Image size cannot exceed 2 MB.')
+		if value.image.height > 4096:
+			raise serializers.ValidationError('Image height cannot exceed 4096px.')
+		if value.image.width > 4096:
+			raise serializers.ValidationError('Image width cannot exceed 4096px.')
+		return value
+
+    def get_is_owner(self, obj):
+        request = self.context.get('request')
+        return request.user == obj.user
