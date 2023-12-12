34cdb3939430a5688d7092ec2b92e49fe8078855
Mthabs <>
2023-12-12 13:27:14 +0000

    add followers list view, create view and detail view

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from django.db.models import Q
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+class FollowerListView(generics.ListAPIView):
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Follower.objects.filter(Q(user=user) | Q(following_user=user))
+
+class FollowerCreateView(generics.CreateAPIView):
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class FollowerDetailView(generics.RetrieveDestroyAPIView):
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Follower.objects.filter(Q(user=user) | Q(following_user=user))
+
+    def perform_destroy(self, instance):
+        if instance.user == self.request.user:
+            instance.delete()
+        else:
+            self.permission_denied(self.request)
