a072cd37abb1a1612c3e306c7bd710d69e08712d
Mthabs <>
2023-12-12 18:45:31 +0000

    add friends list and detailed view classes

--- friends/views.py
+++ friends/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Friend
+from .serializers import FriendSerializer
 
-# Create your views here.
+class FriendListCreateView(generics.ListCreateAPIView):
+    queryset = Friend.objects.all()
+    serializer_class = FriendSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class FriendDetailView(generics.RetrieveDestroyAPIView):
+    queryset = Friend.objects.all()
+    serializer_class = FriendSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def delete(self, request, *args, **kwargs):
+        friend = self.get_object()
+        if request.user == friend.user:
+            return super().delete(request, *args, **kwargs)
+        else:
+            return Response({'detail': 'You do not have permission to delete this friend.'}, status=status.HTTP_403_FORBIDDEN)
